f4b045676cfcdf383df001d23c3e664bc7a579b4
go-bears <go-bears@users.noreply.github.com>
2015-11-09 14:30:13 -0800

    Update week4_extra.py
    added take it further challenges

--- week4_extra.py
+++ week4_extra.py
@@ -3,26 +3,37 @@ Define a function that counts elements the elements in a list or string. (It is 
 """
 
 """
-Write a function that takes a character (i.e. a string of length 1) and returns True if it is a vowel, False otherwise.
-"""
-
-"""
-Write a function translate() that encrypts a phrase into "rövarspråket" (Swedish for "robber's language"). That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
-"""
-
-"""
-Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in a list of numbers. For example, sum([1, 2, 3, 4]) should return 10, and multiply([1, 2, 3, 4]) should return 24.
-    Define a third function that processes (multiply, addition, factorial, etc) the returned outputs of the sum() and multiply() function
-"""
-
-"""
-Define a function reverse() that computes the reversal of a string. For example, reverse("I am testing") should return the string "gnitset ma I".
+Define a function max_of_three() that takes three numbers as arguments via raw_input() and the function returns the largest of them
 """
 
 """
 Define a function is_palindrome() that recognizes palindromes (i.e. words that look the same written backwards). For example, is_palindrome("radar") should return True.
 
 """
+
+"""
+Write a function that takes a character (i.e. a string of length 1) and returns True if it is a vowel, False otherwise.
+
+take it further:
+write a function function that takes in a string via raw_input() 
+returns the total number of vowels that are in the string.
+
+Revise the function so that the function that it outputs the count of each vowel in the string.
+
+"""
+
+"""
+Write a function translate() that encrypts a phrase into "rövarspråket" (Swedish for "robber's language"). That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+
+Take it further:
+write a function that translates strings in "robber's language" back into english:
+"""
+
+"""
+Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in a list of numbers. For example, sum([1, 2, 3, 4]) should return 10, and multiply([1, 2, 3, 4]) should return 24.
+
+take it further:
+Define a third function that processes (multiply, addition, factorial, etc) the returned outputs of the sum() and multiply() function
 
 """
  This function sequence below outputs a 2x2 grid:
