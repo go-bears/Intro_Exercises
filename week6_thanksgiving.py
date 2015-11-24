a1a681c22e870c4a095c7d8ecb15f11f85331cda
go-bears <go-bears@users.noreply.github.com>
2015-11-24 13:45:12 -0800

    Create week6_thanksgiving.py
    exercises featuring functions, loops, while & for loops, string indexes, conditionals, problem solving

---
+++ week6_thanksgiving.py
@@ -0,0 +1,61 @@
+ """1)Write a program that will ask the user for a message and the number of times they want that message displayed. Then output the message that number of times."""
+
+
+
+ """
+ 2) Write a Python program to find those numbers which are divisible by 7 and
+#  multiple of 5, between 1500 and 2700 (both included).
+#  """
+
+
+
+"""
+
+
+3) Given: Two positive integers a and b (a<b<15000). 
+ Return: The sum of all odd integers from a through b, inclusively.
+ """
+
+
+
+
+ """
+ 4)
+ Given: A string s of length at most 200 letters and four integers a, b, c and d.
+ Return: The slice of this string from indices a through b and c through d (with space in between), inclusively.
+
+
+ 72 79 119 125
+
+Solution should return:
+Burhinus daurica
+
+
+string = "g9aB0iza5rvlugFxAQnzxYjzjUVzRjajEJ7P25FcwWHt4VYbOoSgGo5I8H94zOsBP0tAkq3dBurhinusxonARhdcy44zOjdOMyJdz8rqtOmZeimLY9rHkEvdauricalYh6x65Jy3ZFTS3F303GdMZr7YVgwN9o4Ouo"
+
+ """
+
+
+
+
+
+"""
+ mega challenges
+write program that validates that a user has input a valid email address, and doesn't let a person continue until a valid email address is entered
+for example :" melissa@hackbrightacademy.com" is valid and "melissa@hackbrightacademy" or" @hackbrightacademy.com
+" is not valid.
+
+If you want to add complexity to the email validator, have your code return hints to user on what's wrong with their email.
+
+Here are some helper variables:
+
+top_level_domain = ['mil', 'gov', 'edu', 'int', 'net', 'org', 'com']
+email_fail_msg = "this is not a valid email. Please try again. "
+email_pass = "thank you for entering a valid email address!"
+
+Some hints:
+you can try exploring while-loops, and importing the regex (regular expressions) module in python
+
+"""
+
+
