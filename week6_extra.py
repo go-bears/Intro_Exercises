9e579b1bc677336a58c4790de6c3a60ac950c96b
go-bears <go-bears@users.noreply.github.com>
2015-11-24 13:42:47 -0800

    Create week6_extra.py
    extra challenge exercises for week 6: lists, loops, conditionals, string formatting

---
+++ week6_extra.py
@@ -0,0 +1,37 @@
+
+"""
+Define a function procedures input_num() and histogram() that takes asks a user for 3 or more integers, appends the inputs into list of integers and prints a histogram to the screen. 
+
+For example, histogram([4, 9, 7]) should print the following:
+|
+|****
+|*********
+|******
+|
+
+
+learning goals: chaining functions , lists, loops, print statements
+
+"""
+
+
+"""
+Write a function find_longest_word() that takes a list of words from a user and returns the length of the longest one.
+
+learning goals: functions, len(), lists, conditioanls, loops
+"""
+
+
+"""
+A pangram is a sentence that contains all the letters of the English alphabet at least once, for example: The quick brown fox jumps over the lazy dog. Your task here is to write a function to check if a sentence input by a user to see is a pangram or not. 
+
+some panagrams:
+Pack my box with five dozen liquor jugs.
+
+Crazy Fredrick bought many very exquisite opal jewels. 
+
+extra high fives for if you write your own original panagrams!!!
+
+
+learning goals: list operations, loops, raw input() 
+"""
