45050bceb76330274a6499cb18f7c252a3dc9d95
go-bears <melissa.fabros@gmail.com>
2017-01-15 13:54:59 -0800

    remove mortar variable, add large brick wall challenge

--- brickwall_w5.py
+++ brickwall_w5.py
@@ -2,8 +2,8 @@
 Brick Wall exercise:
 
 Functions are used to isolate different small specific tasks.
-Functions abstract action, grouping several actions as a single object, and then you group those actions to create more  bigger and more advanced objects. 
- 
+Functions abstract action, grouping several actions as a single object, and then you group those actions to create more  bigger and more advanced objects.
+
 
 Goal: I've given you "starter functions" of  that create different rows.
 use the individual brick functions to create rows of bricks to build a "strong" wall. You have to design your "row" functions so that the seams do not meet. (there's a sample below)
@@ -13,36 +13,37 @@ Finally, create a small_wall() function that automatically builds the brick wall
 small_wall() is the trigger to build the entire wall, all the functions return outputs until the last function call.
 
 
-Take it further: 
-make a medium_wall() function and a large_wall() function, making sure to use all the brick sizes. 
+Take it further:
+make a medium_wall() function and a large_wall() function, making sure to use all the brick sizes.
 
 
 learning goals: abstraction, chaining functions, return vs. print statements, calling functions, passing values through function parameters and arguments, calling elements from a list, global & local scope
 
 """
-import time
+from time import sleep
 
 bricks = [ "[]", "[__]", "[____]", "[______]" ]
 
 
 
-def row1(brick4, mortar): 
-    mortar = bricks[0]  #sample code, you should replace it
-    brick4 = bricks[4]  #sample code, you can replace it
-    row1 = (mortar + brick4 + mortar) * 3 #sample code, you can replace it
-    brick2 = bricks[2]
+def row1(brick_size1, brick_size2):
+    brick1 = brick_size1 #example code, you can replace it
+    brick2 = brick_size2  #example code, you can replace it
+    row1 = (brick2 + brick2 + brick1) #sample code, you can replace it
     return row1
-    
-def row2(mortar, brick2):
-    mortar = bricks[0] #sample code, you can replace b/c rows don't align right
-    brick2 = bricks[2]
-    row2 = (brick2 + mortar ) * 6
+
+def row2(brick_size1, brick_size2, brick_size3):
+
+    brick1 = brick_size1 #sample code, you should replace b/c rows don't align
+    brick2 =  brick_size2 #sample code, you should replace b/c rows don't align
+    brick3 = brick_size3 #sample code, you should replace b/c rows don't align
+    row2 = brick1  * 6 + brick2 + brick3 #sample code, you should replace b/c rows don't align
     return row2
 
 def row3():
     #do something
     return row3
-    
+
 
 def row4():
     #do something
@@ -55,28 +56,39 @@ def row5():
 
 
 def small_wall():
-    print row1(bricks[3], bricks[0])
-    time.sleep(1)
-    print row2(bricks[0], bricks[2])
-    time.sleep(1)
+    """
+    example of output of small wall. Note how the seams between the bricks
+    meet at in the middle of another brick.
+
+    [__][__][]
+    [][__][__]
+    [__][__][]
+    [][__][__]
+    [__][__][]
+
+    """
+
+    print row1(bricks[0], bricks[1])
+    sleep(1)
+    print row2(bricks[0], bricks[2], bricks[3])
+    sleep(1)
     #call row function with relevant argument(s)
-    time.sleep(1)
+    sleep(1)
     #call row function with relevant argument(s)
-    time.sleep(1)
+    sleep(1)
     #call row function with relevant argument(s)
 
-    
-small_wall()
+def large_wall():
+    """create a function that builds a large wall"""
+    # use the return outputs of the current functions to build a larger wall
+    # do not alter the row functions after you complete the small wall
+    # the small wall and the large wall should both display in the terminal correctly
 
-    
 
-"""
-sample output of a wall. Note how the seams between the bricks meet at in the middle of another brick.
+def main():
+    small_wall()
+    large_wall()
 
-[__][__][]
-[][__][__]
-[__][__][]
-[][__][__]
-[__][__][]
 
-"""
+if __name__ == '__main__':
+    main()
